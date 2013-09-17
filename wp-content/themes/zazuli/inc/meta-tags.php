2ef1c0446084e021d9ee1242cf2e19677dd6fbd1
Virgilio Santos <virgilio.santos@gmail.com>
2013-09-17 19:20:38 -0300

    deleting all wordpress files

--- wp-content/themes/zazuli/inc/meta-tags.php
+++
@@ -1,3 +0,0 @@
-<div class="meta">
-	<p><?php the_author(); ?> as <?php the_time(); ?>, <?php the_date(); ?></p>
-</div>
--- wp-content/themes/zazuli/inc/meta.php
+++
@@ -1,4 +0,0 @@
-<div class="meta">
-	<p><?php the_author_posts_link(); ?> as <?php the_time(); ?>, <?php the_date(); ?><br />
-	<?php comments_popup_link('Nenhum comentario', '1 comentario', '% comentarios', 'comments-link', ''); ?></p>
-</div>
